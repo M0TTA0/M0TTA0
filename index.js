5e9f4808a017ae4aa36dbd755ba0b3e4248e92f4
Usuario Campo Mourao <uept11-user@aluno.pr.senac.br>
2022-07-25 10:59:10 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function exibirNome (){
+    let nome = document.getElementById("c1-entrada").value
+   let frase = "ola, " + nome 
+   document.getElementById("c1-resultado").innerHTML = frase
+}
+
+function calculando (){
+    
+}
